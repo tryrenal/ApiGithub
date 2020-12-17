7d314823013dec7bc23ee9996ffd6272a5060e0b
renaldy <renalraion@gmail.com>
2020-12-17 07:07:38 +0700

    [add] adding searchview

--- app/src/main/java/com/renaldysabdo/apigithub/ui/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/apigithub/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.renaldysabdo.apigithub.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ProgressBar
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
@@ -11,9 +12,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.renaldysabdo.apigithub.R
 import com.renaldysabdo.apigithub.ui.adapter.GithubAdapter
 import com.renaldysabdo.apigithub.ui.adapter.GithubLoadStateAdapter
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 
+@ExperimentalCoroutinesApi
+@FlowPreview
 class MainActivity : AppCompatActivity() {
 
     val mainViewModel : MainViewModel by viewModel()
@@ -25,11 +30,24 @@ class MainActivity : AppCompatActivity() {
 
         val recycler = findViewById<RecyclerView>(R.id.recycler_main)
         val progress = findViewById<ProgressBar>(R.id.pb_main)
+        val search = findViewById<SearchView>(R.id.sv_main)
+        search.setQuery("tryrenal", true)
 
         val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recycler.layoutManager = layoutManager
 
-        mainViewModel.searchUser("stev").observe(this){ data ->
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                mainViewModel.query.value = newText
+                return true
+            }
+        })
+
+        mainViewModel.users.observe(this){ data ->
             lifecycleScope.launch {
                 githubAdapter.submitData(data)
             }
--- app/src/main/java/com/renaldysabdo/apigithub/ui/MainViewModel.kt
+++ app/src/main/java/com/renaldysabdo/apigithub/ui/MainViewModel.kt
@@ -1,9 +1,27 @@
 package com.renaldysabdo.apigithub.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
 import androidx.lifecycle.asLiveData
 import com.renaldysabdo.apigithub.domain.usecase.GithubUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
 
 class MainViewModel (private val usecase: GithubUseCase) : ViewModel(){
-    fun searchUser(name: String) = usecase.searchUser(name).asLiveData()
+    val query : MutableLiveData<String> = MutableLiveData("tryrenal")
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    val users = query.asFlow()
+        .debounce(300)
+        .distinctUntilChanged()
+        .flatMapLatest {
+            searchUser(it)
+        }.asLiveData()
+
+    private fun searchUser(name: String) = usecase.searchUser(name)
 }
