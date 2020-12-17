2712e125cc95958b2bd64497ac550b312c85ac29
renaldy <renalraion@gmail.com>
2020-12-17 22:27:25 +0700

    [update] update loadstate adapter with viewBinding

--- app/src/main/java/com/renaldysabdo/apigithub/ui/adapter/loadstate/GithubLoadStateAdapter.kt
+++ app/src/main/java/com/renaldysabdo/apigithub/ui/adapter/loadstate/GithubLoadStateAdapter.kt
@@ -1,8 +1,10 @@
 package com.renaldysabdo.apigithub.ui.adapter.loadstate
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
+import com.renaldysabdo.apigithub.databinding.LoadStateFooterBinding
 
 class GithubLoadStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<GithubLoadStateViewHolder>() {
     override fun onBindViewHolder(holder: GithubLoadStateViewHolder, loadState: LoadState) {
@@ -13,6 +15,7 @@ class GithubLoadStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<G
         parent: ViewGroup,
         loadState: LoadState
     ): GithubLoadStateViewHolder {
-        return GithubLoadStateViewHolder.create(parent, retry)
+        val itemBinding = LoadStateFooterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GithubLoadStateViewHolder.create(itemBinding, retry)
     }
 }
--- app/src/main/java/com/renaldysabdo/apigithub/ui/adapter/loadstate/GithubLoadStateViewHolder.kt
+++ app/src/main/java/com/renaldysabdo/apigithub/ui/adapter/loadstate/GithubLoadStateViewHolder.kt
@@ -1,42 +1,33 @@
 package com.renaldysabdo.apigithub.ui.adapter.loadstate
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
-import com.renaldysabdo.apigithub.R
+import com.renaldysabdo.apigithub.databinding.LoadStateFooterBinding
 
 class GithubLoadStateViewHolder(
-    view: View,
+    private val binding: LoadStateFooterBinding,
     retry: () -> Unit
-) : RecyclerView.ViewHolder(view) {
-    private val btnRetry : Button = itemView.findViewById(R.id.retry_button)
-    private val errorMsg : TextView = itemView.findViewById(R.id.error_msg)
-    private val progress : ProgressBar = itemView.findViewById(R.id.progress_bar)
+) : RecyclerView.ViewHolder(binding.root) {
 
     init {
-        btnRetry.setOnClickListener { retry.invoke() }
+        binding.retryButton.setOnClickListener { retry.invoke() }
     }
 
     fun bind(loadState: LoadState){
-        if (loadState is LoadState.Error){
-            errorMsg.text = loadState.error.localizedMessage
+        with(binding){
+            if (loadState is LoadState.Error){
+                errorMsg.text = loadState.error.localizedMessage
+            }
+            progressBar.isVisible = loadState is LoadState.Loading
+            retryButton.isVisible = loadState !is LoadState.Loading
+            errorMsg.isVisible = loadState !is LoadState.Loading
         }
-        progress.isVisible = loadState is LoadState.Loading
-        btnRetry.isVisible = loadState !is LoadState.Loading
-        errorMsg.isVisible = loadState !is LoadState.Loading
     }
 
     companion object {
-        fun create(parent: ViewGroup, retry: () -> Unit) : GithubLoadStateViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.load_state_footer, parent, false)
-            return GithubLoadStateViewHolder(view, retry)
+        fun create(binding: LoadStateFooterBinding, retry: () -> Unit) : GithubLoadStateViewHolder {
+            return GithubLoadStateViewHolder(binding, retry)
         }
     }
 }
